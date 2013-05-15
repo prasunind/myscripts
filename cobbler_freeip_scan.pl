aaaacea037043d04a467f84e49d97df4e28720e6
prasunind <firefoxx@gmail.com>
2013-05-15 16:41:48 +0530

    fixed cobbler

--- cobbler_freeip_scan.pl
+++ cobbler_freeip_scan.pl
@@ -1,71 +1,65 @@
 #!/usr/bin/perl
+$mesg="\n\t -h --help \n\t -n --Network fe, data or mgmt\n\t -C --If it's a controller \n\t -c --Number or free ip you need,upto 508 \n \n Usage : ./freeip.pl -n <network subnet name> -C <if it's a controller> -c <No of free ip's you need,upto 508 > \n";
 
 use Getopt::Std;
 use Array::Utils qw(:all);
 
-$COUNT="1";
-$TOTALC="";
+
+
+
 $NETWORK="10.129.";
-@FE="4";
+@FE=(0);
 @FECNT=(1..15);
-@DATA="16";
+@DATA=(16);
 @DATACNT=(17..31);
-@MGMT="32";
+@MGMT=(32);
 @MGMTCNT=(33..47);
 @IPS=(1..254);
-undef $CNT;
+undef $COUNT;
 
 %options=();
 getopts("hn:Cc:", \%options);
 
-print "Usage : ./freeip.pl -n <network subnet name> -C <if it's a controller> -c <No of free ip's you need> \n" if defined $options{h};
 
 $OPT="$options{n}" if  defined $options{n};
 $COUNT="$options{c}" if  defined $options{c};
+print "$mesg" if defined $options{h};
 
 
 if (defined $options{C}){
-        if ( "$options{n}" eq "fe" )   { @NETPARAM="@FECNT"}
-        if ( "$options{n}" eq "data" ) { @NETPARAM="@DATACNT" }
-        if ( "$options{n}" eq "mgmt" ) { @NETPARAM="@MGMTCNT" }
+        if ( "$options{n}" eq "fe" )   { @NETPARAM=@FECNT}
+        if ( "$options{n}" eq "data" ) { @NETPARAM=@DATACNT }
+        if ( "$options{n}" eq "mgmt" ) { @NETPARAM=@MGMTCNT }
         }
 
-if (!defined $options{c}){
-        if ( "$options{n}" eq "fe" )   { @NETPARAM="@FE"}
-        if ( "$options{n}" eq "data" ) { @NETPARAM="@DATA" }
-        if ( "$options{n}" eq "mgmt" ) { @NETPARAM="@MGMT" }
+if (!defined $options{C}){
+        if ( "$options{n}" eq "fe" )   { @NETPARAM=@FE}
+        if ( "$options{n}" eq "data" ) { @NETPARAM=@DATA }
+        if ( "$options{n}" eq "mgmt" ) { @NETPARAM=@MGMT }
         }
 
-$NETID=shift(@NETPARAM);
 
 
+if (defined $options{n}) {$Totalfreeip=master()} else { print "Please check help for correct options \n" if !defined $options{h}};
 
-@ret=get_used_ip ( "$NETID");
-@free=search_free_ip ( \@IPS,  \@ret, $NETID);
-
-
-$TOTALC=$TOTALC + ($#free +1);
-
-if ( $TOTALC >= $COUNT ) {
-	
-	foreach (@free) {
-		print "$_ \n";
-	}
-}else {
-	foreach (@free) {
-                    print "$_ \n \n Scaning next subnet....\n";
-		    $NETID=shift(@NETPARAM);		
-		    @ret=get_used_ip ( "$NETID");
-		    @free=search_free_ip ( \@IPS,  \@ret, $NETID);	
-             }
-
-}	
-	
+if ( $Totalfreeip < $COUNT ) {  $m=master(); print "------ $m ---- \n"; }; 
 
 
 
 
+sub master {
+	$ipv4="0";	
+	$NETID=shift(@NETPARAM);
+	@ret=get_used_ip ( "$NETID");
+	@free=search_free_ip ( \@IPS,  \@ret, $NETID);
+		foreach (@free) {
+                	print "$_ \n";
+			$ipv4++
+        	}
+		print "You have $ipv4 unassigned ip in cobbler for network  10.129.$NETID.0/20\n\n";
+		return $ipv4++;
 
+}
 
 
 
@@ -73,12 +67,15 @@ sub search_free_ip {
 	my  $range=@_[0];
 	my  $used=@_[1];
 	my  $net=@_[2];
+	@freeipno=();
+	@freeip=();
 	@freeipno=array_minus(@$range,@$used);	
+
 		foreach $no (@freeipno) {
 			$ip=join('','10.129.',$net,'.',$no);
 			push(@freeip,$ip);
 	}
-		return @freeip;
+			return @freeip;
 }
 
 sub uniq { my %seen; grep !$seen{$_}++, @_  };
@@ -86,17 +83,20 @@ sub uniq { my %seen; grep !$seen{$_}++, @_  };
 sub get_used_ip {
 
         my $Netid="@_[0]";
+	@ip=();
 
-        open (FH,"<","/root/myscript/cobbler_output") or die "$!: Cobbler File error";
+	system("/usr/bin/cobbler report > /tmp/cobbler");
+        open (FH,"<","/tmp/cobbler") or die "$!: Cobbler File error";
 
         while ($ipadd = <FH>) {
                 chomp $ip;
-                if ($ipadd =~ /\b(10\.0\.$Netid\.)(.*)\b/) {
+                if ($ipadd =~ /\b(10\.129\.$Netid\.)(.*)\b/) {
                 push(@ip,$2);
                 }
         }
         @ip=uniq(@ip);
         @ip=sort {$a <=> $b} @ip;
         return @ip;
+	close FH;
 }
 
